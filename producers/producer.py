9e4ac240948e333b0332bd75fcac75536a9d29ad
christophermccall <camccall24@gmail.com>
2024-05-03 13:14:52 -0400

    updated topic utils

--- producers/producer.py
+++ producers/producer.py
@@ -6,8 +6,6 @@ import random
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda m: dumps(m).encode('ascii'))
 
-producer_2 = KafkaProducer(bootstrap_servers='localhost:9092',
-                           value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 
 
@@ -22,11 +20,5 @@ for i in range(1000):
 
 
 
-def produce(file_path):
-    with open(file_path, 'r') as file:
-        for line in file:
-            j_data = loads(line)
-            producer_2.send(topic='listen-activity', value=j_data)
-            sleep(1)
-        producer_2.close()
+
 

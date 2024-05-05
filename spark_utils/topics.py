de5489b2b462cb0d07624f3e325819d8a0ed85ff
christophermccall <camccall24@gmail.com>
2024-05-05 10:05:22 -0400

    added some obeservabilty to the produce method

--- spark_utils/topics.py
+++ spark_utils/topics.py
@@ -1,8 +1,11 @@
 import os
 from time import sleep
+import logging
 from json import dumps, loads
 from kafka import KafkaProducer
 from kafka.common import TopicAlreadyExistsError
+
+
 
 
 class Topic:
@@ -10,7 +13,20 @@ class Topic:
         self.topic = topic
         self.port = port
         self.file = file
+        self.success = 0
+        self.failure = 0
 
+    def on_send_error(self, excp):
+        self.failure += 1
+        logging.error('failed to send', exc_info=excp)
+        print(self.failure)
+
+    def on_send_success(self, metadata):
+        self.success += 1
+        print(f'Topic: {metadata.topic}')
+        print(f'Partition: {metadata.partition}')
+        print(f'Offset: {metadata.offset}')
+        print(f'messages(rows): {self.success}')
     def j_produce(self):
         producer = KafkaProducer(bootstrap_servers=f'localhost:{self.port}',
                                  value_serializer=lambda x: dumps(x).encode('utf-8'),
@@ -18,11 +34,11 @@ class Topic:
         with (open(self.file, 'r') as file):
             for line in file:
                 j_data = loads(line)
-                producer.send(topic=self.topic, value=j_data)
-                # .add_callback('success') \ research these and find out how to use them properly
-                # .add_errback()
-                #add more informative responses to err and call bak functions, (metadata, offset, and so on)
-           # producer.flush()  # ensures all messages are set before the close
+                producer.send(topic=self.topic, value=j_data) \
+                .add_callback(self.on_send_success) \
+                .add_errback(self.on_send_error)
+                # add more informative responses to err and call bak functions, (metadata, offset, and so on)
+            producer.flush()  # ensures all messages are set before the close
             producer.close()
 
     def create(self, topic, port):

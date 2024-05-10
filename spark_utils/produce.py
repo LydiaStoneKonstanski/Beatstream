2c468d0b92eed42a6b8b4c6212d3b841bef6a6f2
LydiaStoneKonstanski <yourname@example.com>
2024-05-10 15:35:27 -0400

    Jupyter Lab Map in progress

--- spark_utils/produce.py
+++ spark_utils/produce.py
@@ -1,7 +1,7 @@
 from topics import Topic
-listen = Topic('listen-events', 9092, '/Users/chris/Downloads/sample/listen_events.json')
-analysis = Topic('analysis', 9092, '/Users/chris/Downloads/mill_songs/analysis copy.csv')
-tracks = Topic('tracks', 9092, '/Users/chris/Downloads/mill_songs/tracks copy.csv')
+listen = Topic('listen-events', 9092, '/Users/lydia/Projects/Beatstream/data/sample/listen_events')
+analysis = Topic('analysis', 9092, '/Users/lydia/Projects/Beatstream/data/analysis.csv')
+tracks = Topic('tracks', 9092, '/Users/lydia/Projects/Beatstream/data/tracks.csv')
 
 # listen.j_produce()
 # analysis.c_produce()
